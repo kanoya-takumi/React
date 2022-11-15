a6faceb33cb744c24828010e3c2c93623e9dd22d
kanoya-takumi <118144784+kanoya-takumi@users.noreply.github.com>
2022-11-15 17:34:59 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+/*eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import ColofulMessage from "./components/aColofulMessage";
+
+const App = () => {
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColofulMessage color="brown ">元気ですか？</ColofulMessage>
+      <ColofulMessage color="pink">元気です!</ColofulMessage>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( *´艸｀)</p>}
+    </>
+  );
+};
+export default App;
---
+++ src/components/aColofulMessage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const ColofulMessage = (props) => {
+  const { color, children } = props;
+  const contentLedy2Style = {
+    //color: props.color,
+    color: color,
+    fontSize:
+      "18px" /*CSSのようにfont-size: 18px;では書けない.キャメルケースで記入*/
+  };
+  return <p style={contentLedy2Style}>{children}</p>;
+  // return  <p style={contentLedy2Style}>{props.message}</p>;
+};
+export default ColofulMessage;
